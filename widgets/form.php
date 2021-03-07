872982dc8a6d5381a9e7d5c0ec466846e72729e4
Sergey Peshalov <desfpc@gmail.com>
2021-03-07 15:06:45 +0300

    Виджет form - внесение тестовых данных

--- widgets/form.php
+++ widgets/form.php
@@ -6,7 +6,12 @@
  * @var ice\Web\Widget $this
  */
 
+//TODO js валидация (email, int, float, маска, required поле)
+//TODO генерация HTML из массива
+//TODO вывод ошибок (при js и серверной валидации)
+//TODO тестовый массив - обычные чекбоксы, радиобутаны, текстареи, субмит
 
+//Заполнение тестового массива для разработки (и далее документации)
 $formArr = [
     'method' => 'POST',
     'id' => 'testForm',
@@ -25,7 +30,90 @@ $formArr = [
                 'placeholder' => 'Введите email',
                 'value' => '',
                 'required' => true,
-                'help' => 'Является login-ом на сайте. Другие пользователи его не увидят.'
+                'help' => 'Является login-ом на сайте. Другие пользователи его не увидят.',
+                'validator' => 'email',
+            ]
+        ],
+        'row_2' => [
+            [
+                'label' => 'Код товара',
+                'type' => 'input',
+                'name' => 'goodcode',
+                'placeholder' => 'код товара',
+                'value' => '',
+                'required' => false,
+            ],
+            [
+                'label' => 'Стоимость',
+                'type' => 'input',
+                'name' => 'price',
+                'placeholder' => '0',
+                'value' => '',
+                'required' => false,
+                'validator' => 'float'
+            ],
+            [
+                'label' => 'Тест валидатора по маске',
+                'type' => 'input',
+                'name' => 'testMask',
+                'placeholder' => '12.12.2020',
+                'value' => '',
+                'required' => false,
+                'validator' => 'mask',
+                'mask' => ''
+            ]
+        ],
+        'row_3' => [
+            [
+                'label' => 'Язык',
+                'type' => 'select',
+                'name' => 'language',
+                'live-search' => false,
+                'value' => '',
+                'class' => 'selectpicker',
+                'options' => [
+                    [
+                        'name' => 'Русский',
+                        'value' => '1'
+                    ],
+                    [
+                        'name' => 'English',
+                        'value' => '2'
+                    ],
+                    [
+                        'name' => 'English',
+                        'value' => '3'
+                    ]
+                ]
+            ]
+        ],
+        'row_4' => [
+            [
+                'type' => 'input-group',
+                'label' => 'Дата начала и окончания события',
+                'inputs' => [
+                    [
+                        'type' => 'input',
+                        'name' => 'date_event',
+                        'value' => '07.03.2021 13:24',
+                        'placeholder' => 'dd.mm.yyyy hh:ii',
+                        'validator' => 'datetime'
+                    ],
+                    [
+                        'type' => 'input',
+                        'name' => 'date_end',
+                        'value' => '10.03.2021 13:24',
+                        'placeholder' => 'dd.mm.yyyy hh:ii',
+                        'validator' => 'datetime'
+                    ]
+                ]
+            ],
+            [
+                'type' => 'switch ',
+                'label' => 'Важный материал',
+                'name' => 'important',
+                'value' => 1,
+                'checked' => false
             ]
         ]
     ]
