897dc0e819620dffe8d3968cbd3dae9bfaac86ed
Sergey Peshalov <desfpc@gmail.com>
2021-03-07 18:25:46 +0300

    Прототип класса Form

--- vendor/desfpc/ice/Web/Form.php
+++ vendor/desfpc/ice/Web/Form.php
@@ -5,7 +5,7 @@
  * PHP framework and CMS based on it.
  * https://github.com/desfpc/iceCMS
  *
- * TODO: Form Class
+ * Form Class
  *
  */
 
@@ -13,8 +13,31 @@ namespace ice\Web;
 
 class Form
 {
-    public function __construct()
+
+    public $formArr = []; //форма (массив) для виджета
+
+    public function __construct(array $arr)
+    {
+        $this->formArr = $arr;
+        return $this->checkArr();
+    }
+
+    //проверка входящего массива на наличае необходимых полей
+    public function checkArr()
+    {
+        return true;
+    }
+
+    //формирование массива полей формы из объекта (для быстрого создания массива для виджета формы)
+    public function makeFromObj($obj)
     {
 
     }
+
+    //валидация формы (проверка заполнения)
+    public function validate()
+    {
+
+    }
+
 }
