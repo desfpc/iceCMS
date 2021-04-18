aa821d9803ad8ce95e541fa31c50d0d99f959812
Sergey Peshalov <desfpc@gmail.com>
2021-04-18 09:35:08 +0300

    Рефакторинг класса CSRF, добавление методжа вывода <input>

--- vendor/desfpc/ice/Tools/CSRF.php
+++ vendor/desfpc/ice/Tools/CSRF.php
@@ -21,30 +21,33 @@ class CSRF
     private $key;
     private $salt;
 
+    /**
+     * CSRF constructor.
+     * @param Settings $settings
+     * @param string $formName
+     */
     public function __construct(Settings $settings, $formName = '')
     {
         $this->formName = $formName;
         $this->salt = $settings->secret;
-        return $this->makeToken();
+        $this->makeToken();
     }
 
-    //формируем CSRF токен
-    private function makeToken()
-    {
-        $this->makeKey();
-        $this->token = hash('tiger192,3', 'CSRF_' . $this->salt . $this->key);
-        $_SESSION['CSRF_' . $this->key] = $this->token;
-        return true;
+    /**
+     * Выдача действующего токена
+     */
+    public function getToken() {
+        return $this->token;
     }
 
-    //формируем ключ формы (что бы был свой токен на каждую форму)
-    private function makeKey()
-    {
-        $this->key = $this->formName . '_' . time() . '_' . rand(0, 1000);
-    }
-
-    //проверяем CSFR
-    public static function checkCSFR($key, $token)
+    /**
+     * проверяем CSFR
+     *
+     * @param $key
+     * @param $token
+     * @return bool
+     */
+    public static function checkCSFR($key, $token): bool
     {
 
         if (!isset($_SESSION['CSRF_' . $key])) return false;
@@ -53,16 +56,35 @@ class CSRF
         return true;
     }
 
-    //получаем ключ
+    /**
+     * получаем ключ
+     *
+     * @return mixed
+     */
     public function getKey()
     {
         return $this->key;
     }
-
-    //получаем токен
-    public function getToken()
+    
+    /**
+     * формируем CSRF токен
+     *
+     * @return bool
+     */
+    private function makeToken(): bool
     {
-        return $this->token;
+        $this->makeKey();
+        $this->token = hash('tiger192,3', 'CSRF_' . $this->salt . $this->key);
+        $_SESSION['CSRF_' . $this->key] = $this->token;
+        return true;
+    }
+
+    /**
+     * формируем ключ формы (что бы был свой токен на каждую форму)
+     */
+    private function makeKey()
+    {
+        $this->key = $this->formName . '_' . time() . '_' . rand(0, 1000);
     }
 
 }
