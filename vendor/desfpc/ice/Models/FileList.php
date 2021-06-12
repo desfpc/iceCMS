12519fc630d0b07cc32bfc78223b0e1f38bb4c06
Sergey Peshalov <desfpc@gmail.com>
2021-06-12 12:11:28 +0300

    Рефакторинг - анотации методов

--- vendor/desfpc/ice/Models/FileList.php
+++ vendor/desfpc/ice/Models/FileList.php
@@ -12,10 +12,25 @@
 namespace ice\Models;
 
 use ice\DB\DB;
+use ice\Settings\Settings;
 
+/**
+ * Class FileList
+ * @package ice\Models
+ */
 class FileList extends ObjectList
 {
-
+    /**
+     * FileList constructor.
+     *
+     * @param DB $DB
+     * @param null|array $conditions
+     * @param null|array $sort
+     * @param int $page
+     * @param int $perpage
+     * @param int $cachetime
+     * @param null|Settings $settings
+     */
     public function __construct(DB $DB, $conditions = null, $sort = null, $page = 1, $perpage = 20, $cachetime = 0, $settings = null)
     {
         $this->doConstruct($DB, 'files', $conditions, $sort, $page, $perpage, $cachetime, $settings);
